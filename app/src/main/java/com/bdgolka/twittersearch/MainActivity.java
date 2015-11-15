2764281f26558119b7cee1e10d82eafd552893d5
Bdgolka <juliapulova@yandex.ru>
2015-11-15 22:28:03 +0200

    Anonymous inner class saveButtonListener was added

--- app/src/main/java/com/bdgolka/twittersearch/MainActivity.java
+++ app/src/main/java/com/bdgolka/twittersearch/MainActivity.java
@@ -1,8 +1,13 @@
 package com.bdgolka.twittersearch;
 
+import android.app.AlertDialog;
 import android.app.ListActivity;
+import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ImageButton;
@@ -27,14 +32,14 @@ public class MainActivity extends ListActivity {
         setContentView(R.layout.activity_main);
 
         //geting referenses on EditText
-        queryText = (EditText)findViewById(R.id.queryEditText);
-        tagEditText = (EditText)findViewById(R.id.tagEditText);
+        queryText = (EditText) findViewById(R.id.queryEditText);
+        tagEditText = (EditText) findViewById(R.id.tagEditText);
 
         //Getting object SharedPreferences for saved requests
-        savedSearches = getSharedPreferences(SEARCHES,MODE_PRIVATE);
+        savedSearches = getSharedPreferences(SEARCHES, MODE_PRIVATE);
 
         //Save tags in ArrayList and sorting
-        tags = new ArrayList<String>(savedSearches.getAll().keySet());
+        tags = new ArrayList<>(savedSearches.getAll().keySet());
         Collections.sort(tags, String.CASE_INSENSITIVE_ORDER);
 
         //Creating object ArrayAdapter and binding tags to ArrayView
@@ -42,13 +47,46 @@ public class MainActivity extends ListActivity {
         setListAdapter(adapter);
 
         //Listener registration for saving request
-        ImageButton saveButton = (ImageButton)findViewById(R.id.saveButton);
-        //saveButton.setOnItemClickListener(saveButtonListener);
+        ImageButton saveButton = (ImageButton) findViewById(R.id.saveButton);
+
+        saveButton.setOnClickListener(saveButtonListener);
 
         //Listener registration for searching in Twitter
-        //getListView().setOnClickListener(itemClickListener);
+        //getListView().setOnItemClickListener(itemClickListener);
 
         //Set listener who is able to delete or edit request
         //getListView().setOnItemClickListener(itemLongClickListener);
+    }
+
+    //saveButtonListener save pare "tag-request" in SharePraferences
+    public OnClickListener saveButtonListener = new OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            //Create tag, if there is any data in queryEditText and tagEditText
+            if (queryText.getText().length() > 0 && tagEditText.getText().length() > 0) {
+                addTaggedSearch(queryText.getText().toString(), tagEditText.getText().toString());
+                queryText.setText("");//clean quertyEditText
+                tagEditText.setText("");//clean tagEditText
+                ((InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE)).hideSoftInputFromWindow(tagEditText.getWindowToken(), 0);
+            } else // mesage with advise to input reques and tag
+            {
+                //Create AlertDialog Builder
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+
+                //Set dialog window header and message
+                builder.setMessage(R.string.missingMessage);
+
+                //Button OK just close the dialog window
+                builder.setPositiveButton(R.string.ok, null);
+
+                //Create object AlertDialog basis AlertDialogBeilder
+                AlertDialog errorDialog = builder.create();
+                errorDialog.show();// Modal dialog window output
+            }
+        }
+    };
+
+    private void addTaggedSearch(String s, String s1) {
+
     }
 }
